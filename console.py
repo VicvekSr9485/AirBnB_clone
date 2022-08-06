74797e167a05c57c8668a15084b5ff80c9995cc4
PROFAY <Oyewaletimmy01@gmail.com>
2022-08-06 15:45:39 +0000

    update

--- console.py
+++ console.py
@@ -7,7 +7,7 @@ from models import storage
 from models.base_model import BaseModel
 import shlex
 from datetime import datetime
-from models.users import User
+from models.user import User
 from models.state import State
 from models.city import City
 from models.place import Place
