91d9c82a9b2774c9e955585e309692817e6ac88f
PROFAY <Oyewaletimmy01@gmail.com>
2022-08-06 15:58:20 +0000

    debug

--- models/base_model.py
+++ models/base_model.py
@@ -61,7 +61,7 @@ class BaseModel:
 
     def __str__(self):
         """ string format method """
-        return "[{}] ({}) ({})".format(self.__class__.__name__,
+        return "[{}] ({}) {}".format(self.__class__.__name__,
                                        self.id, self.__dict__)
 
     def save(self):
