b041508bb3e384895f229be9750b25dd26f9b49d
PROFAY <Oyewaletimmy01@gmail.com>
2022-08-06 16:36:36 +0000

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,12 @@ from models.base_model import BaseModel
 import json
 import os
 from models.user import User
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
