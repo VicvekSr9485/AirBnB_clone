e1fc2f293a3a2ed7a0379587422304f50643c29b
PROFAY <Oyewaletimmy01@gmail.com>
2022-08-07 13:25:27 +0000

    update test new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python3
-"""
-file storage module
-"""
+"""Store first object"""
 
-from models.base_model import BaseModel
+import models
 import json
-import os
-from models.user import User
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
+
+
+def models_obj_hook(o_dict):
+    """imports BaseModel from models and returns dict"""
+    try:
+        cls = o_dict['__class__']
+    except KeyError:
+        return o_dict
+    else:
+        try:
+            return getattr(models, cls)(**o_dict)
+        except AttributeError:
+            return o_dict
 
 
 class FileStorage:
-    """Classes for file storage models
-    """
+    """created private class attributes"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ Return all objects in the storage
-        """
+        """returns dictionary"""
         return self.__objects
 
     def new(self, obj):
-        """ Create a new object in the storage
-        """
-        self.__objects.update({f"{obj.__class__.__name__}.{obj.id}": obj})
+        """sets the obj with key"""
+        key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects[key] = obj
 
     def save(self):
-        """ Save the object in the storage
-        """
-        new_dict = {}
-        for key, value in self.__objects.items():
-            new_dict[key] = value.to_dict()
-            json.dump(new_dict, open(self.__file_path, "w"))
+        """serializes __objects to the JSON file"""
+        with open(self.__file_path, 'w') as f:
+            jdict = {}
+            for name, obj in self.__objects.items():
+                jdict[name] = obj.to_dict()
+            json.dump(jdict, f)
 
     def reload(self):
-        """ Reload the object in the storage
-        """
-        if os.path.exists(self.__file_path):
-            new_data = json.load(open(self.__file_path, "r"))
-            for key, value in new_data.items():
-                FileStorage.__objects[key] = eval(value['__class__'])(**value)
+        """deserializes the JSON file"""
+        try:
+            with open(self.__file_path, 'r') as f:
+                self.__objects = json.load(f, object_hook=models_obj_hook)
+        except:
+            pass
